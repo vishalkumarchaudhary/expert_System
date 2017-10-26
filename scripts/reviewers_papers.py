21eaeae70db08f17bed6866b1e658550d7a4d910
amishranjan <amishranjanranjan@gmail.com>
2017-10-26 23:19:10 +0530

    random forest implemented, some bugs still exist

--- scripts/reviewers_papers.py
+++ scripts/reviewers_papers.py
@@ -1,8 +1,15 @@
 import pandas
+import sklearn
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np 
 from numpy import *
 
 papers = pandas.read_csv('../datasets/papers/training.txt' , header = None)
 reviewers = pandas.read_csv('../datasets/papers/reviewers.txt' , header = None)
+
+peshi = pandas.read_csv('../datasets/papers/abstracts.txt' , header = None ,delimiter='\n')
+
 
 reviewers = reviewers.values
 
@@ -10,13 +17,41 @@ keys = reviewers[:,1]
 vals = reviewers[:,0]
 author_map = dict(zip(keys,vals))
 
+
 papers = papers.values
 
 authorid = []
 for i in range(papers.shape[0]):
 	authorid = authorid + [author_map[papers[i][0]]]
 
-papers = papers[:,1]
 
-for i in set(authorid):
-	p[i] = i == 
+peshi = peshi.values
+
+papers =  np.concatenate((papers[:,1] , peshi[:,0]) )
+
+
+vectorizer = TfidfVectorizer( decode_error  = 'ignore')
+tfidf = vectorizer.fit_transform(papers) 
+
+clasifier = RandomForestClassifier(n_estimators=20, criterion='gini');
+classifier = clasifier.fit(tfidf[:len(authorid)], authorid);
+
+labels = clasifier.predict(tfidf[len(authorid):])
+
+centroid = []
+num_papers = []
+for i in keys:
+	centroid.append(0)
+	num_papers.append(0.0)
+
+for i in range(len(labels)):
+	centroid[labels[i]] = tfidf[len(authorid) + i] + centroid[labels[i]]
+	num_papers[labels[i]] = num_papers[labels[i]] + 1;
+print centroid[0]
+for i in keys:
+	centroid[i] = map(lambda x: x/num_papers[i], centroid[i])
+
+
+
+
+
