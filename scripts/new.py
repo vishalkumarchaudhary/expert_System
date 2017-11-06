765af262f0c930f8959b05f967ad1cb39255e184
okabe <ashutoshu2411@gmail.com>
2017-11-07 01:12:33 +0530

    defined functions and plotted graphs.

--- scripts/new.py
+++ scripts/new.py
@@ -8,36 +8,39 @@ def using(point = ""):
 	usage = resource.getrusage(resource.RUSAGE_SELF)
 	return '''%s: usertime=%s systime=%s mem=%s mb'''%(point,usage[0],usage[1],(usage[2]*resource.getpagesize())/1000000.0 )
 
-print using('start')
-papers = pandas.read_csv('../datasets/papers/training.txt' , header = None).values
-reviewers = pandas.read_csv('../datasets/papers/reviewers.txt' , header = None).values
-peshi = pandas.read_csv('../datasets/papers/abstracts.txt' , header = None ,delimiter='\n').values
-keys = reviewers[:,1]
-vals = reviewers[:,0]
-author_map = dict(zip(keys,vals))
-author_map_ = dict(zip(vals,keys))
-authorid = []
-for i in range(papers.shape[0]):
-	authorid = authorid + [author_map[papers[i][0]]]
-tmp = hstack((papers[:,1],peshi[:,0]))
-v_papers = TfidfVectorizer(decode_error='ignore', max_features=75, max_df=.8 ,min_df=0.001).fit_transform(tmp).toarray()
-ind = KNeighborsClassifier(n_neighbors=5).fit(v_papers[:len(authorid),], authorid).kneighbors(v_papers[len(authorid):,],50)[1]
-print using('after knn')
-arr=[]
-for i in ind :
-	tmp = []
-	tmp_ = []
-	_tmp = []
-	for j in i:
-		tmp.append(authorid[j])
-	for j in range(len(vals)):
-		tmp_.append(tmp.count(j))
-	for j in range(10):
-		_tmp.append(author_map_[ argmax(tmp_)])
-		tmp_[argmax(tmp_)] = -1
-	arr.append(_tmp)
-print using('stop')
-f = open('out.txt', "w")
-for a in arr:
-	f.write(str(a)[1:-1]+'\n')
-f.close()
+def predictExpert(N):
+	print using('start')
+	papers = pandas.read_csv('../datasets/papers/training.txt' , header = None).values
+	reviewers = pandas.read_csv('../datasets/papers/reviewers.txt' , header = None).values
+	peshi = pandas.read_csv('../datasets/papers/abstracts.txt' , header = None ,delimiter='\n').values
+	keys = reviewers[:,1]
+	vals = reviewers[:,0]
+	author_map = dict(zip(keys,vals))
+	author_map_ = dict(zip(vals,keys))
+	authorid = []
+	for i in range(papers.shape[0]):
+		authorid = authorid + [author_map[papers[i][0]]]
+	tmp = hstack((papers[:,1],peshi[:,0]))
+	v_papers = TfidfVectorizer(decode_error='ignore', max_features=75, max_df=.8 ,min_df=0.001).fit_transform(tmp).toarray()
+	ind = KNeighborsClassifier(n_neighbors=5).fit(v_papers[:len(authorid),], authorid).kneighbors(v_papers[len(authorid):,],N)[1]
+	print using('after knn')
+	arr=[]
+	for i in ind :
+		tmp = []
+		tmp_ = []
+		_tmp = []
+		for j in i:
+			tmp.append(authorid[j])
+		for j in range(len(vals)):
+			tmp_.append(tmp.count(j))
+		for j in range(10):
+			_tmp.append(author_map_[ argmax(tmp_)])
+			tmp_[argmax(tmp_)] = -1
+		arr.append(_tmp)
+	print using('stop')
+	f = open('out.txt', "w")
+	for a in arr:
+		f.write(str(a)[1:-1]+'\n')
+	f.close()
+
+predictExpert(60)
