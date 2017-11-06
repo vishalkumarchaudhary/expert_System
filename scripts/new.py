4642531c18e730bacaac7cc986655d04a8089b83
okabe <ashutoshu2411@gmail.com>
2017-11-07 00:12:01 +0530

    shortened the code.

--- scripts/new.py
+++ scripts/new.py
@@ -1,20 +1,10 @@
 import pandas
-import sklearn
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from numpy import *
-from sklearn.cluster import KMeans
-from sklearn.decomposition import PCA
-from matplotlib import pyplot
-
 papers = pandas.read_csv('../datasets/papers/training.txt' , header = None).values
 reviewers = pandas.read_csv('../datasets/papers/reviewers.txt' , header = None).values
 peshi = pandas.read_csv('../datasets/papers/abstracts.txt' , header = None ,delimiter='\n').values
-
-#print papers
-#print reviewers.shape
-
 keys = reviewers[:,1]
 vals = reviewers[:,0]
 author_map = dict(zip(keys,vals))
@@ -22,19 +12,9 @@ author_map_ = dict(zip(vals,keys))
 authorid = []
 for i in range(papers.shape[0]):
 	authorid = authorid + [author_map[papers[i][0]]]
-
 tmp = hstack((papers[:,1],peshi[:,0]))
-
-v_papers = TfidfVectorizer(decode_error='ignore', max_features=75, max_df=.5 ,min_df=0.001).fit_transform(tmp).toarray()
-
-#print(v_papers[:,len(authorid)].shape)
-
-classifier = KNeighborsClassifier(n_neighbors=5)
-
-classifier.fit(v_papers[:len(authorid),] , authorid )
-
-(neig , ind )= classifier.kneighbors(v_papers[len(authorid):,],100)
-print len(vals)
+v_papers = TfidfVectorizer(decode_error='ignore', max_features=75, max_df=.8 ,min_df=0.001).fit_transform(tmp).toarray()
+ind = KNeighborsClassifier(n_neighbors=5).fit(v_papers[:len(authorid),], authorid).kneighbors(v_papers[len(authorid):,],50)[1]
 arr=[]
 for i in ind :
 	tmp = []
@@ -42,15 +22,10 @@ for i in ind :
 	_tmp = []
 	for j in i:
 		tmp.append(authorid[j])
-
-
 	for j in range(len(vals)):
 		tmp_.append(tmp.count(j))
-	tmp_ = array(tmp_)
-
-	for j in range(5):
+	for j in range(10):
 		_tmp.append(author_map_[ argmax(tmp_)])
 		tmp_[argmax(tmp_)] = -1
 	arr.append(_tmp)
-
-print(arr)
+print arr
